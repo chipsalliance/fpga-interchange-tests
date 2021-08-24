9b260a7c69c61f12a35b2be85b800894cc9597e7
Maciej Kurc <mkurc@antmicro.com>
2021-08-24 11:42:38 +0200

    Added parsing simulation and validation target statuses
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- utils/report_targets.py
+++ utils/report_targets.py
@@ -20,111 +20,161 @@ import os
 
 
 BUILT_TARGET_RE = \
-    re.compile(r"^\[\s*[0-9]+\%\]\s+Built\starget\s(?P<target>.*)")
+    re.compile(r".*Built\starget\s(?P<target>.*)")
 
 FAILED_TARGET_RE = \
     re.compile(r".*recipe\sfor\starget\s'(?P<target>.*)'\sfailed")
 
+all_stages = [
+    "json",
+    "netlist",
+    "phys",
+    "fasm",
+    "bit",
+    "bit-fasm",
+    "dcp",
+    "dcp-bit",
+    "dcp-bit-fasm",
+    "dcp-diff-fasm",
+    "vvp",
+    "sim",
+    "post-synth-vvp",
+    "post-synth-sim",
+    "fasm2bels",
+    "fasm2bels-bit",
+    "fasm2bels-fasm",
+    "fasm2bels-bit-fasm",
+    "fasm2bels-dcp",
+    "fasm2bels-diff-fasm",
+]
+
+
 # =============================================================================
+
+
+def identify_target(target):
+    """
+    Extracts names of architecture, board, design and stage given the
+    executed target name.
+    """
+
+    if target.startswith("sim-test-"):
+        target = target.replace("sim-test-", "")
+
+        arch = "?"
+
+        if target.startswith("post-synth-"):
+            target = target.replace("post-synth-", "")
+            pfx = "post-synth-"
+        else:
+            pfx = ""
+
+        fields = target.split("-")
+        if len(fields) == 2:
+            design, board = fields
+            stage = "sim"
+
+        elif len(fields) == 3:
+            design, board, stage = fields
+
+        else:
+            return None
+
+        stage = pfx + stage
+
+    else:
+
+        fields = target.split("-", maxsplit=3)
+        if len(fields) < 4:
+            return None
+
+        arch, design, board, stage = fields
+        if stage not in all_stages:
+            return None
+
+    return arch, design, board, stage
 
 
 def main():
     
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "log",
+        "--log",
         type=str,
+        nargs="+",
         help="Build log"
     )
     parser.add_argument(
-        "csv",
+        "--csv",
         type=str,
         help="Output CSV report"
     )
 
     args = parser.parse_args()
 
-    # Read the log
-    with open(args.log, "r") as fp:
-        log = fp.readlines()
-
-    # Scan successfully built targets and failed targets
     built_targets = set()
     failed_targets = set()
 
-    for line in log:
-        line = line.strip()
+    # Read logs
+    for log_name in args.log:
 
-        # Built
-        match = BUILT_TARGET_RE.fullmatch(line)
-        if match is not None:
-            built_targets.add(match.group("target"))
+        # Read log
+        with open(log_name, "r") as fp:
+            log = fp.readlines()
 
-        # Failed
-        match = FAILED_TARGET_RE.match(line)
-        if match is not None:
-            path = match.group("target").split(os.path.sep)
-            temp = [p for p in path if p.endswith(".dir")]
-            if not temp:
-                continue
+        # Scan successfully built targets and failed targets
+        for line in log:
+            line = line.strip()
 
-            target = temp[0].replace(".dir", "")
-            failed_targets.add(target)
+            # Built
+            match = BUILT_TARGET_RE.fullmatch(line)
+            if match is not None:
+                built_targets.add(match.group("target"))
+
+            # Failed
+            match = FAILED_TARGET_RE.match(line)
+            if match is not None:
+                path = match.group("target").split(os.path.sep)
+                temp = [p for p in path if p.endswith(".dir")]
+                if not temp:
+                    continue
+
+                target = temp[0].replace(".dir", "")
+                failed_targets.add(target)
 
     # Sanity check
     temp = built_targets & failed_targets
     assert not temp, temp
 
     # Identify tests and their statuses
-    all_stages = [
-        "json",
-        "netlist",
-        "phys",
-        "fasm",
-        "bit",
-        "bit-fasm",
-        "dcp",
-        "dcp-bit",
-        "dcp-bit-fasm",
-        "dcp-diff-fasm",
-    ]
-
     designs = dict()
 
     for target in built_targets:
 
-        fields = target.split("-", maxsplit=3)
-        if len(fields) < 4:
+        fields = identify_target(target)
+        if not fields:
             continue
 
         arch, design, board, stage = fields
-        if stage not in all_stages:
-            continue
-
         print("V", arch, design, board, stage)
 
         key = (arch, design, board)
         if key not in designs:
             designs[key] = dict()
-
         designs[key][stage] = True
 
     for target in failed_targets:
 
-        fields = target.split("-", maxsplit=3)
-        if len(fields) < 4:
+        fields = identify_target(target)
+        if not fields:
             continue
 
         arch, design, board, stage = fields
-        if stage not in all_stages:
-            continue
-
         print("X", arch, design, board, stage)
 
         key = (arch, design, board)
         if key not in designs:
             designs[key] = dict()
-
         designs[key][stage] = False
 
     # Add stages not reported in the log as unknown
@@ -132,6 +182,28 @@ def main():
         for stage in all_stages:
             if stage not in designs[key]:
                 designs[key][stage] = None
+
+    # Aggregate data
+    for key, stages in list(designs.items()):
+        arch, design, board = key
+
+        if arch == "?":
+            del designs[key]
+
+            # Merge statuses by replacing unknown with known
+            for key2, stages2 in designs.items():
+                if (design, board) == key2[1:]:
+
+                    for stage, v2 in list(stages2.items()):
+                        v1 = stages[stage]
+                        if v1 is None and v2 is not None:
+                            designs[key2][stage] = v2
+                        elif v1 is not None and v2 is None:
+                            designs[key2][stage] = v1
+                        elif v1 is None and v2 is None:
+                            pass
+                        else:
+                            assert False, (key2, stage, v1, v2)
 
     # Output report
     with open(args.csv, "w") as fp:

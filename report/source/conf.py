a2cc703db0075560e5469691cf92c4ea78a79777
Maciej Kurc <mkurc@antmicro.com>
2021-08-27 15:52:20 +0200

    Added missing license headers
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- report/source/conf.py
+++ report/source/conf.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
+# Copyright (C) 2021  The SymbiFlow Authors.
+#
+# Use of this source code is governed by a ISC-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/ISC
+#
+# SPDX-License-Identifier: ISC
+
 # symbiflow-arch-defs documentation build configuration file, created by
 # sphinx-quickstart on Thu Oct 18 14:29:14 2018.
 #

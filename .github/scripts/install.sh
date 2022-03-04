b6eec8ce8f8df4c96ef103b451b7860dfee56db4
Alessandro Comodi <acomodi@antmicro.com>
2022-03-04 13:29:45 +0100

    WIP
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- .github/scripts/install.sh
+++ .github/scripts/install.sh
@@ -12,7 +12,17 @@ set -e
 
 du -ah ${INSTALL_DIR}
 export GIT_HASH=$(git rev-parse --short HEAD)
+
+echo "========================="
+echo "Compressing techmap files"
+echo "========================="
+mkdir -p ${INSTALL_DIR}/techmaps
+cp tests/common/libs/*.v ${INSTALL_DIR}/techmaps
 tar -I "pixz" -cvf interchange-techmaps-${GIT_HASH}.tar.xz -C ${INSTALL_DIR} techmaps
+
+echo "======================="
+echo "Compressing device data"
+echo "======================="
 for device in $(ls ${INSTALL_DIR}/devices)
 do
     tar -I "pixz" -cvf interchange-$device-${GIT_HASH}.tar.xz -C ${INSTALL_DIR}/devices $device

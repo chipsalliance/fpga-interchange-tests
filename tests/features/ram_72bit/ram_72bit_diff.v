c076ccbe64c62e8568f85e09bfcf467c4bd53a3a
Krzysztof Boronski <kboronski@antmicro.com>
2022-03-30 13:22:03 -0500

    xcup: BRAM tests * RAMB18E2 (ram, ram_36bit) * RAMB36E2 (ram_72bit)
    Signed-off-by: Krzysztof Boronski <kboronski@antmicro.com>

--- tests/features/ram_72bit/ram_72bit_diff.v
+++ tests/features/ram_72bit/ram_72bit_diff.v
@@ -1,4 +1,4 @@
-// Copyright (C) 2022  The F4PGA Authors.
+// Copyright (C) 2022  The Symbiflow Authors.
 //
 // Use of this source code is governed by a ISC-style
 // license that can be found in the LICENSE file or at
@@ -6,29 +6,26 @@
 //
 // SPDX-License-Identifier: ISC
 
-module top_zcu104 (
+module top_diff (
     input wire clk_p,
     input wire clk_n,
-    output wire [15:0] led,
-    input wire [15:0] sw,
-    output wire tx,
     input wire rx,
+    output wire tx,
+    input wire [15:0] sw,
+    output wire [15:0] led,
     input wire butu,
     input wire butd,
     input wire butl,
     input wire butr,
     input wire butc
 );
-
-    // The ZCU104 CLK comes from a differential pair
     wire clk;
     IBUFDS ibuf_ds (.I(clk_p), .IB(clk_n), .O(clk));
-
     top top_ (
-        .led(led),
-        .sw(sw),
-        .tx(tx),
         .rx(rx),
+        .tx(tx),
+        .sw(sw),
+        .led(led),
         .butu(butu),
         .butd(butd),
         .butl(butl),
